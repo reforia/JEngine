4f71af6341f837d0d3601d6f61ecb7fedc9b628b
reforia <tianmouz@smu.edu>
2020-09-22 00:27:14 +0800

    Added Transform Matrix to shader

--- JEngine/src/Private/Renderer/Renderer.cpp
+++ JEngine/src/Private/Renderer/Renderer.cpp
@@ -15,10 +15,11 @@ namespace JEngine
 
 	}
 
-	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray)
+	void Renderer::Submit(const std::shared_ptr<Shader>& shader, const std::shared_ptr<VertexArray>& vertexArray, const glm::mat4& transform)
    	{
 		shader->Bind();
 		shader->UploadUniformMat4("u_ViewProjection", m_SceneData->ViewProjectionMatrix);
+		shader->UploadUniformMat4("u_Transform", transform);
 
 		vertexArray->Bind();
 		RenderCommand::DrawIndexed(vertexArray);
--- Sandbox/src/Sandbox.cpp
+++ Sandbox/src/Sandbox.cpp
@@ -2,6 +2,8 @@
 
 #include "imgui/imgui.h"
 
+#include "glm/gtc/matrix_transform.hpp"
+
 class ExampleLayer : public JEngine::Layer
 {
 public:
@@ -40,6 +42,7 @@ public:
 			layout(location = 1) in vec4 a_Color;
 			
 			uniform mat4 u_ViewProjection;
+			uniform mat4 u_Transform;
 
 			out vec3 v_Position;	
 			out vec4 v_Color;
@@ -48,7 +51,7 @@ public:
 			{
 				v_Position = a_Position;
 				v_Color = a_Color;
-				gl_Position = u_ViewProjection * vec4(v_Position, 1.0);
+				gl_Position = u_ViewProjection * u_Transform * vec4(v_Position, 1.0);
 			}
 		)";
 
@@ -98,7 +101,17 @@ public:
 
 		JEngine::Renderer::BeginScene(m_Camera);
 
-		JEngine::Renderer::Submit(m_Shader, m_VertexArray);
+		static glm::mat4 scale = glm::scale(glm::mat4(1.0f), glm::vec3(0.1f));
+
+		for (int y = 0; y < 20; y++)
+		{
+			for (int x = 0; x < 20; x++)
+			{
+				glm::vec3 pos(x * 0.11f, y * 0.11f, 0.0f);
+				glm::mat4 trans = glm::translate(glm::mat4(1.0f), pos) * scale;
+				JEngine::Renderer::Submit(m_Shader, m_VertexArray, trans);
+			}
+		}
 
 		JEngine::Renderer::EndScene();
 	}
